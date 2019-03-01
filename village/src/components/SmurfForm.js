c221da6bc71fd0165786ba1251a15521bc40ed03
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-01 13:53:59 -0500

    Styled button on <form>

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -41,7 +41,7 @@ class SmurfForm extends React.Component {
   render() {
     return (
       <div className="smurf-form-container">
-        <h2 className="smurf-form-header">Introduce a New Smurf to Smurf Village!</h2>
+        <h2 className="smurf-form-header">Introduce a New Smurf!</h2>
         <form className="smurf-form" onSubmit={this.handleSubmit}>
           <input
             onChange={this.handleInputChange}
